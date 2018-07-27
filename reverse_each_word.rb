1c7e0f6f05c24e79333ed9e89acd5ab554b1428d
Eli Cho <eli.cho@gmail.com>
2018-07-27 08:42:17 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,6 +9,6 @@ end
 
 def reverse_each_word(sentence)
   new_sentence = sentence.split(" ")
-  new_sentence.collect! {|word| word.reverse}
-  return new_sentence.join(" ")
+  return new_sentence.collect! {|word| word.reverse}
+  
 end
