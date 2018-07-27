6bea5b88630f206bd057beb60b6075f6690b817e
Eli Cho <eli.cho@gmail.com>
2018-07-27 08:45:29 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
-  new_sentence = sentence.split(" ")
+  new_sentence1 = sentence.split(" ")
   collector = []
-  new_sentence.each do |word|
+  new_sentence1.each do |word|
     collector << word.reverse
   end
   return collector.join(" ")
